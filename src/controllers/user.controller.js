70f1723ea0d80d90d9d787a927e65f71c9d8db42
vidhiwhere <vidhikumari6025@gmail.com>
2026-03-07 21:11:35 +0530

    add user.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,29 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {Apierrors} from "../utils/Apierrors.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok"
-  });
-});
+  //get user data from frontend 
+  //validation - not empty 
+  //check if user already exists:usrname, email
+  //check for images, check for avatar
+  //upload them to cloudinary,avatar
+  //create user object - create entry in db 
+  // remove password and refresh toke field from response 
+  // check if user created succesfullu
+  //return response
+
+
+  const { username, email, password, fullName } = req.body;
+
+  console.log("email",email);
+
+ if(
+  [fullName, username, email, password].some((field)=>)
+ ){
+
+ }
+
+})
+
 
 export { registerUser };
